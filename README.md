0e96452c00893b8345db3c4f3a16a816fc1a863f
Lorenzo <lsandini@gmail.com>
2023-05-05 09:23:03 +0300

    just adding

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # just an API for posting notes to an express app and storing them in a mongodb database
 
+added stuff
--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,6 @@
 
 git add .
 git commit -m "just adding"
-git push origin main
+# git push origin main
+
+git request-pull https://git.ko.xz/project main
